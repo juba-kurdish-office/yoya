6f58df2d331e2992c7efded0e08bab0eabb9c544
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-04-12 18:00:39 +0300

    Update profile.js

--- commands/economy/profile.js
+++ commands/economy/profile.js
@@ -1,5 +1,6 @@
-const Discord = require("discord.js");
-const { match } = require("../../functions.js")
+const { MessageEmbed } = require("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
 
 module.exports = {
 	help: {
